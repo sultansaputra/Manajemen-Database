160c026b072c4d9ada133b5e350b73f8d6340a75
sultansaputra <sltnadjiesptra@gmail.com>
2025-06-17 13:26:58 +0800

    Update 2_Create_Document.js

--- 2_Create_Document.js
+++ 2_Create_Document.js
@@ -1,5 +1,11 @@
-db.produk.insertOne({
-  jenis: "Pertalite",
-  harga: 10000000,
-  stok_liter: 1000,
-});
+db.nama.insertMany([
+  {
+    jenis: "Pertalite",
+    harga: 10000000,
+    stok_liter: 1000,
+  },
+  {
+    jenis: "Pertamax",
+    harga: 12400000,
+    stok_liter: 1000,
+  });
