f4359d98f9f2461584f0445da88d6e122d7147dc
sultansaputra <sltnadjiesptra@gmail.com>
2025-06-17 13:28:54 +0800

    Update 2_Create_Document.js

--- 2_Create_Document.js
+++ 2_Create_Document.js
@@ -5,6 +5,11 @@ db.nama.insertMany([
     stok_liter: 1000,
   },
   {
+    jenis: "Solar",
+    harga: 68000000,
+    stok_liter: 1000,
+  },
+  {
     jenis: "Pertamax",
     harga: 12400000,
     stok_liter: 1000,
